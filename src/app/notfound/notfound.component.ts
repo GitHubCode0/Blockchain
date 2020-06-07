f435c5030d778f17452a28ac319a871ad00d345a
Alexis Jennings <alexisjennings@Alexiss-MacBook-Air.local>
2020-06-08 01:08:17 +0930

    build complete

---
+++ src/app/notfound/notfound.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start',
+  templateUrl: './notfound.component.html',
+  styleUrls: ['./notfound.component.css']
+})
+export class NotFoundComponent {
+
+}
